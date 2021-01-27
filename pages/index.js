d878ee3315a3737ba3e3f8b41fff14ffd6a2623f
gcarniel <gpcarniel@outlook.com>
2021-01-26 21:57:53 -0300

    Códigos após finalizar aula 2.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Footer from '../src/components/Footer'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+ 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-
-// const BackgoundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer =styled.div`
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -24,17 +21,36 @@ export const QuizContainer =styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <QuizLogo />
+      <Head>
+        <title>Quiz GC</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>Titulo</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Detalhes...</p>
+            <form onSubmit={function(infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}>
+              <input 
+                onChange={function(infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Me diz seu nome" 
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -46,7 +62,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl={'https://github.com/gcarniel'}/>
+      <GitHubCorner projectUrl="https://github.com/gcarniel" />
     </QuizBackground>
-  )
+  );
 }
